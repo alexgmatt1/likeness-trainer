9aeacafbfbb17a12b8e438db37cd5a441b918efc
tayo4pf <tyoluranti@gmail.com>
2023-02-04 22:55:03 +0000

    Added dbmanager.py database manager class Modified backend.py with endpoints Need to refactor to accept corrent frontend input and output

--- backend.py
+++ backend.py
@@ -1,14 +1,15 @@
 from flask import Flask
 import psycopg2
 import pandas as pd
-from flask import jsonify,request, render_template
+from flask import jsonify, request, render_template
 from flask_cors import CORS, cross_origin
+from dbmanager import DbManager as dbm
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  return "hello"
 
 @app.route("/test")
 def test():
@@ -21,6 +22,42 @@ def getImages():
   df = pd.read_csv("./frontend/public/assets/files.csv")
   print(df)
   return jsonify({"images": list(df.itertuples(index=False))})
+
+@app.route("/checkRegistered", methods = ['POST'])
+@cross_origin()
+def checkRegistered():
+  username = request.form['username']
+  with dbm() as db:
+    registered = db.user_exists(username)
+  return registered
+
+@app.route("/recoverVotes", methods = ['POST'])
+@cross_origin()
+def recoverVotes():
+  username = request.form['username']
+  with dbm() as db:
+    votes = db.get_votes(username)
+  return jsonify({"votes": list(votes)})
+
+@app.route("/addUser", methods = ['POST'])
+@cross_origin()
+def addUserInfo():
+  username = request.form['username']
+  age = request.form['age']
+  gender = request.form['gender']
+  with dbm() as db:
+    db.add_user(username, age, gender)
+  return
+
+@app.route("/submitVotes", methods = ['POST'])
+@cross_origin()
+def submitVotes():
+  username = request.form['username']
+  votes = request.form['votes']
+  with dbm() as db:
+    for vote in votes:
+      db.add_vote(username, vote[0], vote[1])
+  return
 
 
 if __name__ == "__main__":
