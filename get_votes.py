ed6afa18edcd2d6327f28fb136b919dae7aa03ef
spdskatr <25078279+spdskatr@users.noreply.github.com>
2023-02-07 11:49:48 +0000

    Add get_votes script

---
+++ get_votes.py
@@ -0,0 +1,28 @@
+import psycopg2
+import pandas as pd
+import datetime
+
+from login import LOGIN_KWARGS
+
+def get_cols(cur):
+    cur.execute("select column_name from information_schema.columns where table_name='votes' order by table_name, ordinal_position")
+    return [x for (x,) in cur.fetchall()]
+
+def get_vote_df(cur):
+    cur.execute("select * from votes")
+    votes = cur.fetchall()
+    cols = get_cols(cur)
+    data = []
+    for vote in votes:
+        assert len(vote) == len(cols)
+        res = {}
+        for name, val in zip(cols, vote):
+            res[name] = val
+        data.append(res)
+    return pd.DataFrame(data)
+
+with psycopg2.connect(**LOGIN_KWARGS) as conn2:
+    with conn2.cursor() as cur2:
+        votes = get_vote_df(cur2)
+
+print(votes)
