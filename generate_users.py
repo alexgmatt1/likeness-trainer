49d0eedbadc5fd386e86d91bb933001e6e5918b4
spdskatr <25078279+spdskatr@users.noreply.github.com>
2023-02-14 11:15:58 +0000

    Add generate-users script

---
+++ generate_users.py
@@ -0,0 +1,29 @@
+import random
+import psycopg2
+
+from login import LOGIN_KWARGS
+
+assert False and "This script has already run. Do not run it again unless we desperately need more users. Also there's no code that deals with conflicts with existing access codes in the database."
+
+def get_access_code():
+    res = [str(random.randint(0, 9)) for _ in range(8)]
+    return "".join(res)
+    
+s = set()
+
+for _ in range(1000):
+    c = get_access_code()
+    while c in s:
+        c = get_access_code()
+    s.add(c)
+
+ss = sorted(s)
+
+with psycopg2.connect(**LOGIN_KWARGS) as conn2:
+    with conn2.cursor() as cur2:
+        cur2.executemany("INSERT INTO users(username) VALUES (%s)", [(x,) for x in ss])
+
+    conn2.commit()
+
+for x in ss:
+    print(x)
