7d418a0840ffc26f7bd7d727064569506994b861
spdskatr <25078279+spdskatr@users.noreply.github.com>
2023-02-27 15:48:03 +0000

    Add image for clip embbedding script

---
+++ pass_images_through_clip.py
@@ -0,0 +1,23 @@
+import torch
+import clip
+import os
+import pickle
+from tqdm import tqdm
+
+from PIL import Image
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+res = []
+with torch.no_grad():
+    for f in tqdm(os.listdir("output")):
+        if f.endswith(".png"):
+            img = preprocess(Image.open(f"output/{f}")).unsqueeze(0).to(device)
+            img_features = model.encode_image(img)
+            res.append({ "img": f, "features": list(img_features.numpy().flatten()) })
+
+with open("res.pkl", "wb") as f:
+    pickle.dump(res, f)
+
+print("Dumped output to res.pkl")
